086f21ebc5fbdee133188f7f75759a2623b3b4bf
flyotlin <flyotlin@gmail.com>
2021-12-30 14:35:15 +0800

    feat: set up MySQL and environment variables

--- url_shortener/url_handler/models.py
+++ url_shortener/url_handler/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Urls(models.Model):
+    shortened_url = models.CharField(max_length=12)
+    full_url = models.CharField(max_length=512)
+    create_at = models.DateTimeField()
+    update_at = models.DateTimeField()
+
+    class Meta:
+        # managed = False
+        db_table = 'urls'
--- url_shortener/url_shortener/settings.py
+++ url_shortener/url_shortener/settings.py
@@ -11,17 +11,23 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5yen0l6_k+4v!bd5*cnn-p^b_ghjv7y9$-f3bqad91cyf827j-'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -79,8 +85,15 @@ WSGI_APPLICATION = 'url_shortener.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': env('DB_NAME'),
+        'ENGINE': 'mysql.connector.django',
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
+        'OPTIONS': {
+            'autocommit': True,
+        },
     }
 }
 
@@ -88,11 +101,11 @@ DATABASES = {
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://redis:6379/1",
+        "LOCATION": env('REDIS_LOCATION'),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient"
         },
-        "KEY_PREFIX": "url_shortener"
+        "KEY_PREFIX": env('REDIS_KEY_PREFIX')
     }
 }
 
